929a7763671e5f0789bf3fea50add95703407547
Uladzislau Paulovich <selatnick@gmail.com>
2018-12-22 14:12:39 +0300

    Fix first render bug

--- piano/script.js
+++ piano/script.js
@@ -1,45 +1,50 @@
+var id = 0;
+var canvas = null;
+var context = null;
+
+function renderNotes() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  context.lineWidth = 5;
+  for (var a = 0; a < 5; ++a) {
+    context.beginPath();
+    context.moveTo(0.1 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
+    context.lineTo(0.9 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
+    context.stroke();
+  }
+
+  var noteId = Math.floor(Math.random() * 11);
+  context.beginPath();
+  var arcCenterX = 0.4 * canvas.width + 0.2 * canvas.width * id;
+  var arcCenterY = 0.15 * canvas.height + 0.05 * canvas.height * noteId;
+  var noteRadius = 0.034 * canvas.width;
+  context.arc(arcCenterX, arcCenterY, noteRadius, 0, 2 * Math.PI);
+  context.moveTo(arcCenterX + noteRadius, arcCenterY);
+  var topY = arcCenterY - 0.21 * canvas.height;
+  if (noteId < 5) {
+    topY = arcCenterY + 0.21 * canvas.height;
+  }
+  context.lineTo(arcCenterX + noteRadius, topY);
+  context.stroke();
+
+  id += 1;
+  id %= 2;
+}
+
 function startApp() {
   var tempo = document.getElementById("tempo").value;
   var element = document.getElementById("tempo-menu");
   element.parentNode.removeChild(element);
 
-  var canvas = document.createElement("canvas");
+  canvas = document.createElement("canvas");
   var minSide = Math.min(document.documentElement.clientWidth,
                          document.documentElement.clientHeight);
   canvas.width = minSide;
   canvas.height = minSide;
   document.body.insertBefore(canvas, document.body.childNodes[0]);
 
-  var context = canvas.getContext("2d");
+  context = canvas.getContext("2d");
 
-  var id = 0;
-  window.setInterval(function() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    context.lineWidth = 5;
-    for (var a = 0; a < 5; ++a) {
-      context.beginPath();
-      context.moveTo(0.1 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
-      context.lineTo(0.9 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
-      context.stroke();
-    }
-
-    var noteId = Math.floor(Math.random() * 11);
-    context.beginPath();
-    var arcCenterX = 0.4 * canvas.width + 0.2 * canvas.width * id;
-    var arcCenterY = 0.15 * canvas.height + 0.05 * canvas.height * noteId;
-    var noteRadius = 0.034 * canvas.width;
-    context.arc(arcCenterX, arcCenterY, noteRadius, 0, 2 * Math.PI);
-    context.moveTo(arcCenterX + noteRadius, arcCenterY);
-    var topY = arcCenterY - 0.21 * canvas.height;
-    if (noteId < 5) {
-      topY = arcCenterY + 0.21 * canvas.height;
-    }
-    context.lineTo(arcCenterX + noteRadius, topY);
-    context.stroke();
-
-    id += 1;
-    id %= 2;
-  }, 1000 * 60 / tempo);
-
+  window.setInterval(renderNotes, 1000 * 60 / tempo);
+  renderNotes();
 }
