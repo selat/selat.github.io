1551c9c1973edf0fc54192dbe039fe81aabdf4a5
Uladzislau Paulovich <selatnick@gmail.com>
2018-12-22 04:50:20 +0300

    Some refactoring

--- piano/script.js
+++ piano/script.js
@@ -4,33 +4,37 @@ function startApp() {
   element.parentNode.removeChild(element);
 
   var canvas = document.createElement("canvas");
-  canvas.width = 500;
-  canvas.height = 500;
+  var minSide = Math.min(document.documentElement.clientWidth,
+                         document.documentElement.clientHeight);
+  canvas.width = minSide;
+  canvas.height = minSide;
   document.body.insertBefore(canvas, document.body.childNodes[0]);
 
   var context = canvas.getContext("2d");
 
   var id = 0;
   window.setInterval(function() {
-    context.clearRect(0, 0, 500, 500);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
     context.lineWidth = 5;
     for (var a = 0; a < 5; ++a) {
       context.beginPath();
-      context.moveTo(50, 50 * a + 100);
-      context.lineTo(450, 50 * a + 100);
+      context.moveTo(0.1 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
+      context.lineTo(0.9 * canvas.width, 0.1 * canvas.height * a + 0.2 * canvas.height);
       context.stroke();
     }
 
     var noteId = Math.floor(Math.random() * 11);
     context.beginPath();
-    context.arc(200 + 100 * id, 75 + 25 * noteId, 17, 0, 2 * Math.PI);
-    context.moveTo(200 + 100 * id + 17, 75 + 25 * noteId);
-    var topY = 75 + 25 * noteId - 105;
+    var arcCenterX = 0.4 * canvas.width + 0.2 * canvas.width * id;
+    var arcCenterY = 0.15 * canvas.height + 0.05 * canvas.height * noteId;
+    context.arc(arcCenterX, arcCenterY, 17, 0, 2 * Math.PI);
+    context.moveTo(arcCenterX + 17, arcCenterY);
+    var topY = arcCenterY - 105;
     if (noteId < 5) {
-      topY = 75 + 25 * noteId + 105;
+      topY = arcCenterY + 105;
     }
-    context.lineTo(200 + 100 * id + 17, topY);
+    context.lineTo(arcCenterX + 17, topY);
     context.stroke();
 
     id += 1;
